b6ddecb5e1656bdd290979eec7e4dbdf11230509
yuvrajrajput22 <rajputyuvraj2216@gmail.com>
2026-07-16 23:46:30 +0530

    Added Lombok dependency and used in User Entity

--- bookmyseat/src/main/java/com/bookmyseat/bookmyseat/entity/User.java
+++ bookmyseat/src/main/java/com/bookmyseat/bookmyseat/entity/User.java
@@ -2,8 +2,9 @@ package com.bookmyseat.bookmyseat.entity;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import jakarta.persistence.*;
+import lombok.Data;
 
-
+@Data
 @Entity
 public class User {
 
@@ -15,55 +16,4 @@ public class User {
     private String email;
     private String password;
     private String role;
-
-    @JsonCreator
-    public User(){}
-
-    public User(Long id, String name, String email, String password, String role) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
